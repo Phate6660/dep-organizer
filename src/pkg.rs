21cb3adaba755d00b51ca317519791696c0401a2
ValleyKnight <ValleyKnight@protonmail.com>
2021-06-16 16:03:53 -0400

    do some restructuring to make adding things easier, add WIP (untested) support for xbps

--- src/pkg.rs
+++ src/pkg.rs
@@ -1,5 +1,5 @@
 use std::io::{stdout, Write};
-use std::process::Stdio;
+use std::process::{exit, Stdio};
 
 fn run(cmd: &str, args: &[&str], message: &str) {
     let child = std::process::Command::new(cmd)
@@ -11,14 +11,53 @@ fn run(cmd: &str, args: &[&str], message: &str) {
     stdout().write_all(&child.stdout).unwrap();
 }
 
-fn backend(package_manager: &str, package: &str, operation: &str) {
-    if package_manager == "portage"{
-        if operation == "install" {
+fn match_op(operation: &str, cmd: &str, package: &str) {
+    match operation {
+        "install" => {
+            let args: Vec<&str> = match cmd {
+                "emerge" => vec!("-a", "-t", "-v", &package),
+                "xbps-install" => vec!("-S", &package),
+                _ => vec!("N/A"),
+            };
+            if args[0] == "N/A" {
+                println!("You found a bug, it shouldn't be possible to reach this but I had to cover this to make rust happy.");
+                exit(1);
+            }
             let message = ["Could not install ", &package].concat();
-            run("emerge", &["-a", "-t", "-v", &package], &message);
-        } else if operation == "remove" {
+            run(cmd, &args, &message);
+        },
+        "remove" => {
+            let cmd = if cmd == "xbps-install" {
+                "xbps-remove"
+            } else {
+                cmd
+            };
+            let args: Vec<&str> = match cmd {
+                "emerge" => vec!("-a", "-v", "-c", &package),
+                "xbps-remove" => vec!(&package),
+                _ => vec!("N/A"),
+            };
+            if args[0] == "N/A" {
+                println!("You found a bug, it shouldn't be possible to reach this but I had to cover this to make rust happy.");
+                exit(1);
+            }
             let message = ["Could not remove ", &package].concat();
-            run("emerge", &["-a", "-v", "-c", &package], &message);
+            run(cmd, &args, &message);
+        },
+        _ => {
+            println!("Sorry, {} is not a supported operation right now.", operation);
+            exit(1);
+        }
+    }
+}
+
+fn backend(package_manager: &str, package: &str, operation: &str) {
+    match package_manager {
+        "portage" => match_op(operation, "emerge", package),
+        "xbps" => match_op(operation, "xbps-install", package),
+        _ => {
+            println!("Sorry, {} is not a supported package manager right now.", package_manager);
+            exit(1);
         }
     }
 }
